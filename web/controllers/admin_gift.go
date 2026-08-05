e26fc052afccf9de9a36d9e01cc3eee410d9ec06
boyback <duxingfly@163.com>
2026-08-06 00:52:16 +0800

    feat: 管理员管理gift功能增删改查

--- web/controllers/admin_gift.go
+++ web/controllers/admin_gift.go
@@ -4,20 +4,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"lottery-study/comm"
+	"lottery-study/models"
 	"lottery-study/services"
 	"lottery-study/web/utils"
+	"lottery-study/web/viewmodels"
+	"time"
 
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/mvc"
 )
 
 type AdminGiftController struct {
-	Ctx     iris.Context
-	Service services.GiftService
+	Ctx         iris.Context
+	ServiceGift services.GiftService
 }
 
 func (c *AdminGiftController) Get() mvc.Result {
-	datalist := c.Service.GetList()
+	datalist := c.ServiceGift.GetList()
 	for i, giftInfo := range datalist {
 		// 奖品发放的计划数据
 		prizedata := make([][2]int, 0)
@@ -54,27 +57,122 @@ func (c *AdminGiftController) Get() mvc.Result {
 	}
 }
 func (c *AdminGiftController) GetEdit() mvc.Result {
+
+	id := c.Ctx.URLParamIntDefault("id", 0)
+	giftInfo := viewmodels.ViewGift{}
+	if id > 0 {
+		data := c.ServiceGift.Get(id)
+		giftInfo.Id = data.Id
+		giftInfo.Title = data.Title
+		giftInfo.PrizeNum = data.PrizeNum
+		giftInfo.PrizeCode = data.PrizeCode
+		giftInfo.PrizeTime = data.PrizeTime
+		giftInfo.Img = data.Img
+		giftInfo.Displayorder = data.Displayorder
+		giftInfo.Gtype = data.Gtype
+		giftInfo.Gdata = data.Gdata
+		giftInfo.TimeBegin = comm.FormatFromUnixTime(int64(data.TimeBegin))
+		giftInfo.TimeEnd = comm.FormatFromUnixTime(int64(data.TimeEnd))
+	}
 	return mvc.View{
 		Name: "admin/giftEdit.html",
 		Data: iris.Map{
 			"Title":   "管理后台",
 			"Channel": "gift",
+			"Info":    giftInfo,
 		},
 		Layout: "admin/layout.html",
 	}
 }
 
 func (c *AdminGiftController) PostSave() mvc.Result {
+	data := viewmodels.ViewGift{}
+	err := c.Ctx.ReadForm(&data)
+	if err != nil {
+		fmt.Println("admin gift postsave readform error=", err)
+		return mvc.Response{
+			Text: err.Error(),
+		}
+	}
+	giftInfo := models.LtGift{}
+	giftInfo.Id = data.Id
+	giftInfo.Title = data.Title
+	giftInfo.PrizeNum = data.PrizeNum
+	giftInfo.PrizeCode = data.PrizeCode
+	giftInfo.PrizeTime = data.PrizeTime
+	giftInfo.Img = data.Img
+	giftInfo.Displayorder = data.Displayorder
+	giftInfo.Gtype = data.Gtype
+	giftInfo.Gdata = data.Gdata
+	t1, err1 := comm.ParseTime(data.TimeBegin)
+	t2, err2 := comm.ParseTime(data.TimeEnd)
+	if err1 != nil || err2 != nil {
+		return mvc.Response{
+			Text: fmt.Sprintf("err1: %s err2: %s", err1.Error(), err2.Error()),
+		}
+	}
+	giftInfo.TimeBegin = int(t1.Unix())
+	giftInfo.TimeEnd = int(t2.Unix())
+	if giftInfo.Id > 0 {
+		datainfo := c.ServiceGift.Get(giftInfo.Id)
+		if datainfo != nil && datainfo.Id > 0 {
+			//奖品数量变化
+			if datainfo.PrizeNum != giftInfo.PrizeNum {
+				giftInfo.LeftNum = datainfo.LeftNum - datainfo.PrizeNum - giftInfo.PrizeNum
+				if giftInfo.LeftNum < 0 || giftInfo.PrizeNum <= 0 {
+					giftInfo.LeftNum = 0
+				}
+			}
+			//奖品周期变化
+			if datainfo.PrizeTime != giftInfo.PrizeTime {
+
+			}
+			giftInfo.SysUpdated = int(time.Now().Unix())
+			c.ServiceGift.Update(&giftInfo, []string{""})
+		} else {
+			giftInfo.Id = 0
+		}
+	}
+	if giftInfo.Id > 0 {
+		giftInfo.LeftNum = giftInfo.PrizeNum
+		giftInfo.SysIp = comm.ClientIP(c.Ctx.Request())
+		giftInfo.SysCreated = int(time.Now().Unix())
+		c.ServiceGift.Create(&giftInfo)
+	}
 	return mvc.Response{
 		Path: "/admin/gift",
 	}
 }
 func (c *AdminGiftController) GetDelete() mvc.Result {
+	id := c.Ctx.URLParamIntDefault("id", 0)
+	if id < 1 {
+		fmt.Println("找不到id")
+		return mvc.Response{
+			Text: "找不到id",
+		}
+	} else {
+		err := c.ServiceGift.Delete(id)
+		if err != nil {
+			fmt.Println("删除失败")
+			return mvc.Response{
+				Text: "删除失败",
+			}
+		}
+	}
 	return mvc.Response{
 		Path: "/admin/gift",
 	}
 }
 func (c *AdminGiftController) GetReset() mvc.Result {
+	id := c.Ctx.URLParamIntDefault("id", 0)
+	if id < 1 {
+		fmt.Println("找不到id")
+		return mvc.Response{
+			Text: "找不到id",
+		}
+	} else {
+		c.ServiceGift.Update(&models.LtGift{Id: id, SysStatus: 0}, []string{"sys_status"})
+	}
 	return mvc.Response{
 		Path: "/admin/gift",
 	}
